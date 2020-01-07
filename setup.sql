2549684b39970d64b4d4aa36853fa94d8b8c9451
Emmanuel Munoz <em66@uw.edu>
2020-01-07 13:49:22 -0800

    Completed DB setup file

--- setup.sql
+++ setup.sql
@@ -1,20 +1,27 @@
+DROP TABLE IF EXISTS tag;
 DROP TABLE IF EXISTS resource;
 DROP TABLE IF EXISTS category;
 
 CREATE TABLE category(
   id   INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE resource(
-  id          NOT NULL AUTO_INCREMENT
+  id          INT NOT NULL AUTO_INCREMENT,
   name        VARCHAR(50) NOT NULL,
-  link        VARCHAR(1000) NOT NULL,
+  link        VARCHAR(1000),
   description TEXT NOT NULL,
   icon        VARCHAR(100) NOT NULL,
-  category_id INT NOT NULL,
   status      VARCHAR(20) NOT NULL DEFAULT "STANDBY",
-  PRIMARY KEY(id),
-  FOREIGN KEY(category_id) REFERENCES category(id)
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE tag(
+   category_id INT NOT NULL,
+   resource_id INT NOT NULL,
+   PRIMARY KEY(category_id, resource_id),
+   FOREIGN KEY(category_id) REFERENCES category(id),
+   FOREIGN KEY(resource_id) REFERENCES resource(id)
 );
